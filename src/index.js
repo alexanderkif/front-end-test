68ff2521c491bcbac0ec3a22c904e09e78b099e4
Aleksandr Nikiforov <alexander_kif@mail.ru>
2019-04-26 09:48:43 +0400

    align all blocks to left-top

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ function importAll(r) {
 require.context('./', true, /\.scss$/);
 importAll(require.context('./components/', true, /\.js$/));
 require.context('./img/', true, /\.(png|ico)$/);
+
+window.onload = () => {
+    alert('window.onload: Документ и все ресурсы загружены');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    alert('DOMContentLoaded: DOM готов');
+});
+
+$(() => {
+    alert('JQuery: DOMContentLoaded');
+});
